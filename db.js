eb2dfe5e1068ede788e19c372112c2326a877fcc
Peerawit789 <6631501087@lamduan.mfu.ac.th>
2025-08-26 21:47:07 +0700

    database com1

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2");//susu
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'expenses'
 });
-module.exports = con; //เสร็จ
+module.exports = con; //เสร็จ//
