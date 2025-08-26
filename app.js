48392503f89041efb43e80e024d8f7906d8f394d
Peerawit789 <6631501087@lamduan.mfu.ac.th>
2025-08-26 22:48:35 +0700

    Add feature login

--- app.js
+++ app.js
@@ -7,6 +7,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// login
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const sql = "SELECT id, password FROM users WHERE username = ?";
+    con.query(sql, [username], function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        if (results.length === 0) {
+            return res.status(401).send("Wrong password or username");
+        }
+
+        bcrypt.compare(password, results[0].password, function(err, same) {
+            if(err) {
+                return res.status(500).send("Hashing error");
+            }
+            if(same) {
+                // ส่ง id กลับไปให้ client เอาไป query /expenses
+                return res.json({ user_id: results[0].id });
+            }
+            return res.status(401).send("Wrong password or username");
+        });
+    })
+});
+
 // ---------- Server starts here ---------
 const PORT = 3000;
 app.listen(PORT, () => {
